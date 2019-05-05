02bf0caa8251899384925e46ed56a2ea9183b42d
mnprsctr <ricardomoratomateos@outlook.com>
2019-05-05 17:53:49 +0200

    Initialize repository

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/index.js'
+    ],
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|vue)$/,
+                use: 'eslint-loader',
+                enforce: 'pre'
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "index.html",
+            inject: true
+        })
+    ]
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './App.vue'
+
+Vue.use(Vuex)
+
+/* eslint-disable-next-line no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
