c144d05852ce4fee65fa5a91df966e1cf89a0ed4
mnprsctr <ricardomoratomateos@outlook.com>
2019-05-05 18:50:36 +0200

    Load vue-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.use(Vuex)
+Vue.use(VueRouter)
 
 /* eslint-disable-next-line no-new */
 new Vue({
